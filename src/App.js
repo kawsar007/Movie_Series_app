47925c934e94c09e12fd9af9da089beeb61c10d4
kawsar007 <imkawsar007@gmail.com>
2020-10-08 13:53:38 +0600

    Project Start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer () {
+    return (
+        <div>
+            <h2>This is Footer Component</h2>
+        </div>
+    )
+}
